4298d14a37530676592d4140f21b1117a9be090b
alberto <alberto@LAPTOP-7R9E1PIM>
2021-12-30 21:58:12 +0000

    Kata 6 - Version 6 (Revised)

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 import toyproducts.models.*;
 import business.ToyBusiness;
+import toyproducts.Toy;
 
 public class Main {
 
@@ -16,87 +17,54 @@ public class Main {
         System.out.println("2 - Rama Asiatica");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        ToyBusiness toyBusiness = null;
+        ArrayList<Toy> carArray = new ArrayList<>();
+        ArrayList<Toy> helicopterArray = new ArrayList<>();
 
         if (input.equals("1")) {
-            System.out.println("Introduzca comando:");
-            input = scanner.nextLine();
             AmericanToyBusiness americanBranch = new AmericanToyBusiness();
-            ToyBusiness toyBusiness = new ToyBusiness(americanBranch);
-            ArrayList<AmericanCarToy> carArray = new ArrayList<>();
-            ArrayList<AmericanHelicopterToy> helicopterArray = new ArrayList<>();
-            
-            while (true) {
-                switch (input) {
-                    case "car":
-                        AmericanCarToy car = (AmericanCarToy) toyBusiness.produceToy("car");
-                        carArray.add(car);
-                        System.out.println("Built cars: "
-                                + carArray.stream()
-                                        .map(c -> c.getSerialNumber().toString())
-                                        .collect(Collectors.joining(", ")));
-                        break;
-
-                    case "helicopter":
-                        AmericanHelicopterToy helicopter = (AmericanHelicopterToy) toyBusiness.produceToy("helicopter");
-                        helicopterArray.add(helicopter);
-                        System.out.println("Built helicopters: "
-                                + helicopterArray.stream()
-                                        .map(c -> c.getSerialNumber().toString())
-                                        .collect(Collectors.joining(", ")));
-                        break;
-
-                    case "exit":
-                        System.out.println("Exiting...");
-                        exit(0);
-
-                    default:
-                        System.out.println("Command unknown");
-                        break;
-
-                }
-                input = scanner.nextLine();
-            }
+            toyBusiness = new ToyBusiness(americanBranch);
         } else if (input.equals("2")) {
-            System.out.println("Introduzca comando:");
-            input = scanner.nextLine();
             AsianToyBusiness asianBranch = new AsianToyBusiness();
-            ToyBusiness toyBusiness = new ToyBusiness(asianBranch);
-            ArrayList<AsianCarToy> carArray = new ArrayList<>();
-            ArrayList<AsianHelicopterToy> helicopterArray = new ArrayList<>();
-            while (true) {
-                switch (input) {
-                    case "car":
-                        AsianCarToy car = (AsianCarToy) toyBusiness.produceToy("car");
-                        carArray.add(car);
-                        System.out.println("Built cars: "
-                                + carArray.stream()
-                                        .map(c -> c.getSerialNumber().toString())
-                                        .collect(Collectors.joining(", ")));
-                        break;
-
-                    case "helicopter":
-                        AsianHelicopterToy helicopter = (AsianHelicopterToy) toyBusiness.produceToy("helicopter");
-                        helicopterArray.add(helicopter);
-                        System.out.println("Built helicopters: "
-                                + helicopterArray.stream()
-                                        .map(c -> c.getSerialNumber().toString())
-                                        .collect(Collectors.joining(", ")));
-                        break;
-
-                    case "exit":
-                        System.out.println("Exiting...");
-                        exit(0);
-
-                    default:
-                        System.out.println("Command unknown");
-                        break;
-
-                }
-                input = scanner.nextLine();
-            }
+            toyBusiness = new ToyBusiness(asianBranch);
         } else {
-            System.out.println("Rama desconocida");
+            System.out.println("Rama no existente");
             exit(0);
+        }
+
+        System.out.println("Introduzca comando:");
+        input = scanner.nextLine();
+
+        while (true) {
+            switch (input) {
+                case "car":
+                    Toy car = toyBusiness.produceToy("car");
+                    carArray.add(car);
+                    System.out.println("Built cars: "
+                            + carArray.stream()
+                                    .map(c -> c.getSerialNumber().toString())
+                                    .collect(Collectors.joining(", ")));
+                    break;
+
+                case "helicopter":
+                    Toy helicopter = toyBusiness.produceToy("helicopter");
+                    helicopterArray.add(helicopter);
+                    System.out.println("Built helicopters: "
+                            + helicopterArray.stream()
+                                    .map(c -> c.getSerialNumber().toString())
+                                    .collect(Collectors.joining(", ")));
+                    break;
+
+                case "exit":
+                    System.out.println("Exiting...");
+                    exit(0);
+
+                default:
+                    System.out.println("Command unknown");
+                    break;
+
+            }
+            input = scanner.nextLine();
         }
     }
 }
--- src/toyproducts/Toy.java
+++ src/toyproducts/Toy.java
@@ -3,5 +3,6 @@ package toyproducts;
 public interface Toy {
     public void pack();
     public void label();
+    public Integer getSerialNumber();
     
 }
